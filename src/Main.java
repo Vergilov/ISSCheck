29df34eed54b5b19f0796aabbae60cfa10b1aa25
Vergilov <biggohunter@gmail.com>
2019-02-27 20:01:35 +0100

    Adding .gitignore and new class ConnectionWithCelestrak to stream input from website Celestrak

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello Project!");
+    public static void main(String[] args) throws Exception {
+        ConnectionWithCelestrak connectionWithCelestrak= new ConnectionWithCelestrak();
+        connectionWithCelestrak.print();
     }
 }
